3df2a74be7b75d40e62d95a708c11175e855e222
Pepelapuerta <sergio_1999_cs@hotmail.com>
2025-08-04 11:17:51 +0200

    Stand By

--- mi-proyecto/src/app/page.tsx
+++ mi-proyecto/src/app/page.tsx
@@ -1,12 +1,9 @@
 import Tareas from "@/components/TareasPersonales/Tareas";
 
-
 export default function Home() {
   return (
-    <main className="min-h-screen bg-gradient-to-br from-slate-900 via-purple-900 to-slate-900 relative overflow-hidden">
-      <div className="absolute inset-0">
+    <main className="min-h-screen bg-gradient-to-br from-slate-900 via-purple-900 to-slate-900">
       <Tareas/>
-      </div>
     </main>
   );
 }
